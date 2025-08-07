a2c845818e3f5aa88ee51f40eada9b73c7fbb3f9
Md. Muntaha Islam <muntaha.islam0019@gmail.com>
2025-08-07 19:16:03 +0600

    Day 2 - Added constructor

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player();
-        p1.id = 1;
-        p1.username = "Alpha";
-        p1.rating = 1500;
+        Player p1 = new Player(1, "Alpha", 1500);
+        Player p2 = new Player(2, "Beta", 1400);
 
-        Player p2 = new Player();
-        p2.id = 2;
-        p2.username = "Beta";
-        p2.rating = 1400;
-
-        System.out.println(p1.username + " (" + p1.rating + ")");
-        System.out.println(p2.username + " (" + p2.rating + ")");
+        p1.printStats();
+        p2.printStats();
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,14 @@ public class Player {
     int id;
     String username;
     int rating;
+
+    public Player(int id, String username, int rating) {
+        this.id = id;
+        this.username = username;
+        this.rating = rating;
+    }
+
+    public void printStats() {
+        System.out.println("Name: " + username + ", Rating: " + rating);
+    }
 }
